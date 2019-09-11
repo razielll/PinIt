b93543792cdfd990fc911fe65af0b04676d61557
Mario <mario.saliba98@gmail.com>
2019-09-11 20:12:01 +0300

    refactor a bit

--- src/App.js
+++ src/App.js
@@ -2,64 +2,45 @@ import React, { Component } from 'react';
 import Geocode from 'react-geocode';
 import './App.scss';
 import { AddressInput, Map, ListItems } from './component/index';
-import { saveToLocalStorage, loadFromLocalStorage, isItems } from './utils';
+import { translateAndSave, fetchFromStorage, checkInStorage } from './utils';
+import { ADDRESS_STORAGE_KEY } from './constants';
 
 Geocode.setApiKey('AIzaSyCKgE8gvERM__LG9dDbOLpAgWoHEqYtZGI');
 
 class App extends Component {
 	state = {
-		items: isItems() ? loadFromLocalStorage() : [],
+		items: checkInStorage(ADDRESS_STORAGE_KEY) ? fetchFromStorage(ADDRESS_STORAGE_KEY) : [],
 		markers: [],
 	};
 
 	handleKeyPress = e => {
-		const {
-			target: { value: addressStr },
-		} = e;
+		const { target: { value: addressStr } } = e;
 		if (e.key === 'Enter') {
 			this.handleSaveItem(addressStr);
 		}
 	};
 
-	handleSaveItem = value => {
-		Geocode.fromAddress(value).then(
-			response => {
-				console.log('GeoCode response ->', response);
-				const result = response.results[0];
-				const { formatted_address } = result;
-				const [street, city, country] = formatted_address.split(', ');
-				const { lat, lng } = result.geometry.location;
+	handleSaveItem = async value => {
+		const response = await translateAndSave(value);
 
-				const item = {
-					address: formatted_address,
-					street,
-					city,
-					country,
-					coords: { lat, lng },
-				};
-				const isSuccessSave = saveToLocalStorage(item);
-				if (isSuccessSave) {
-					console.log('%cSUCCESS SIR', 'color:red');
-					this.setState({
-						items: JSON.parse(localStorage.getItem('list-items')),
-					});
-				}
-			},
-			error => {
-				console.error(error);
-			}
-		);
-	};
+		if (response.success) {
+			this.setState({
+				items: fetchFromStorage(ADDRESS_STORAGE_KEY)
+			});
+		};
+	}
 
 	render() {
+		const { address, items } = this.state;
+
 		return (
 			<div>
 				<AddressInput
-					address={this.state.address}
+					address={address}
 					onKeyPress={this.handleKeyPress}
 				/>
 				<div className='site-content-wrapper'>
-					<ListItems items={this.state.items} />
+					<ListItems items={items} />
 					<Map
 						googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBe2G6Moqh0Zft5OUifEqwKb3TtRkvboxA&v=3.exp&libraries=geometry,places'
 						loadingElement={<div style={{ height: `100%` }} />}
--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,63 @@
-export function saveToLocalStorage(obj) {
-	const currList = localStorage.getItem('list-items')
-		? JSON.parse(localStorage.getItem('list-items'))
-		: [];
+import Geocode from 'react-geocode';
+import { ADDRESS_STORAGE_KEY } from './constants';
 
-	const itemToSave = {
+// saves the new address object to local storage
+export function saveAddress(newAddress) {
+	let currentAddressList = [];
+
+	if (checkInStorage(ADDRESS_STORAGE_KEY)) {
+		currentAddressList = fetchFromStorage(ADDRESS_STORAGE_KEY)
+	}
+
+	const address = {
 		id: new Date().getTime(),
 		icon: '',
-		...obj,
+		...newAddress,
 	};
 
-	const newItems = [...currList, itemToSave];
-	console.log('newItems ->', newItems);
+	const newAddressList = [...currentAddressList, address];
 
 	try {
-		localStorage.setItem('list-items', JSON.stringify(newItems));
+
+		localStorage.setItem(ADDRESS_STORAGE_KEY, JSON.stringify(newAddressList));
 		return true;
 	} catch (e) {
 		return false;
 	}
 }
 
-export function loadFromLocalStorage() {
-	return JSON.parse(localStorage.getItem('list-items'));
+export function fetchFromStorage(key) {
+	return JSON.parse(localStorage.getItem(key));
 }
 
-export function isItems() {
-	return !!JSON.parse(localStorage.getItem('list-items'));
+// need a better name for this
+export function checkInStorage(key) {
+	return !!JSON.parse(localStorage.getItem(key));
 }
 
-export function generateRandomID() {
-	return Math.floor((Math.random() * 9999999) | 0);
+export async function translateAndSave(address) {
+	return Geocode.fromAddress(address).then(
+		({ results }) => {
+			const { formatted_address } = results[0];
+			const [street, city, country] = formatted_address.split(',').map(item => item.trim());
+			const { lat, lng } = results[0].geometry.location;
+
+			const item = {
+				address: formatted_address,
+				street,
+				city,
+				country,
+				coords: { lat, lng },
+			};
+
+			const isSaved = saveAddress(item);
+
+			return { success: isSaved };
+		},
+		error => {
+			console.error(error);
+			return { success: false };
+
+		}
+	);
 }

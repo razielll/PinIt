9ab6ca5a676ced850b871eb0d52315595cf90f5a
razielll <raizel@antons-air.home>
2019-09-11 21:18:34 +0300

    merged and marker console style lol

--- src/component/Map/index.jsx
+++ src/component/Map/index.jsx
@@ -10,9 +10,9 @@ import {
 const Map = withScriptjs(
 	withGoogleMap(props => (
 		<GoogleMap defaultZoom={8} defaultCenter={{ lat: -30.397, lng: 30.644 }}>
-			{props.markers.map(mark => {
-				console.log('one dya i will be marker', mark);
-				return <Marker position={{ lat: mark.lat, lng: mark.lng }} />;
+			{props.markers.map((mark, i) => {
+				console.log('%cMarker ->', 'padding:8px 4px; color:steelblue;', mark);
+				return <Marker key={i} position={{ lat: mark.lat, lng: mark.lng }} />;
 			})}
 		</GoogleMap>
 	))

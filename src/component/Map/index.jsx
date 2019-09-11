546110070374305c8920783f97fc3dd3dea33b63
razielll <raizel@antons-air.home>
2019-09-11 20:16:41 +0300

    MARIO IS THERE A MAP

--- src/component/Map/index.jsx
+++ src/component/Map/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import {
 	withScriptjs,
@@ -9,8 +10,10 @@ import {
 const Map = withScriptjs(
 	withGoogleMap(props => (
 		<GoogleMap defaultZoom={8} defaultCenter={{ lat: -30.397, lng: 30.644 }}>
-			{props.markers.map(mark => console.log('one dya i will be marker', mark))}
-			<Marker position={{ lat: -30.397, lng: 30.644 }} />
+			{props.markers.map(mark => {
+				console.log('one dya i will be marker', mark);
+				return <Marker position={{ lat: mark.lat, lng: mark.lng }} />;
+			})}
 		</GoogleMap>
 	))
 );

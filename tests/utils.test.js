9fbf6cb13c96244f1928d74211a82270aa479ddf
Mario <mario.saliba98@gmail.com>
2019-09-12 13:39:59 +0300

    adds basic testing

---
+++ tests/utils.test.js
@@ -0,0 +1,34 @@
+const { checkInStorage, saveAddress } = require('../src/utils')
+const { ADDRESS_STORAGE_KEY } = require('../src/constants')
+require('jest-localstorage-mock');
+
+localStorage.clear()
+
+test('check if storage is empty', () => {
+    const isInStorage = checkInStorage(ADDRESS_STORAGE_KEY)
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(isInStorage).toBeFalsy();
+});
+
+test('check if item was added to the storage', () => {
+    const newAddress = {
+        address: 'Stella maris 26, Tel aviv, Israel',
+        street: 'Stella maris 26',
+        city: 'Tel aviv',
+        country: 'Israel',
+        coords: {
+            lat: 34.453443,
+            lng: 33.242343
+        },
+    }
+
+    saveAddress(newAddress)
+
+    const savedAddress = JSON.parse(localStorage.__STORE__[ADDRESS_STORAGE_KEY])[0]
+
+    expect(localStorage.getItem).toHaveBeenCalledTimes(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(savedAddress.address).toBe(newAddress.address)
+
+})
